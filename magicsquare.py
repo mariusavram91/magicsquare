aa4d8bdf01061120bf3897fabba94aee9f068b61
Marius Avram <me@mariusavram.com>
2018-08-31 19:40:12 +0200

    Add is_magic_square

--- magicsquare.py
+++ magicsquare.py
@@ -25,8 +25,7 @@ def create_magic_square(n):
 def display_magic_square(magic_square):
     n = len(magic_square)
 
-    print()
-    print("Magic square is (dimension n=" + str(n) + "):")
+    print("\nMagic square is (dimension n=" + str(n) + "):")
 
     for row in magic_square:
         for number in row:
@@ -35,7 +34,24 @@ def display_magic_square(magic_square):
 
 
 def is_magic_square(magic_square):
-    return False
+    size = len(magic_square)
+
+    flat_magic_square = [number for row in magic_square for number in row]
+
+    if not all(isinstance(element, int) for element in flat_magic_square):
+        return False
+    if len(set(flat_magic_square)) != len(flat_magic_square):
+        return False
+
+    sums = []
+    sums.extend([sum(row) for row in magic_square])
+    sums.extend([sum(col) for col in zip(*magic_square)])
+    sums.append(sum(magic_square[i][i] for i in range(size)))
+    sums.append(sum(magic_square[i][size-i-1] for i in range(size)))
+
+    if len(set(sums)) > 1:
+        return False
+    return True
 
 
 def magic_square():
@@ -68,8 +84,12 @@ class MagicSquareValidityTest(unittest.TestCase):
         magic_square = [[1, 1, 2], [4, 5, 6], [7, 8, 9]]
         self.assertFalse(is_magic_square(magic_square))
 
-    def test_square_is_correct(self):
+    def test_square_is_invalid_because_of_sums(self):
         magic_square = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.assertFalse(is_magic_square(magic_square))
+
+    def test_square_is_correct(self):
+        magic_square = [[8, 1, 6], [3, 5, 7], [4, 9, 2]]
         self.assertTrue(is_magic_square(magic_square))
 
 if __name__ == '__main__':
